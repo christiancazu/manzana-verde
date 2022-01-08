87d008ee7f98197c68470fd66ce8f72d07254032
Christian Carrillo <christiancazu@gmail.com>
2022-01-08 05:06:46 -0500

    init front-end

---
+++ vue.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  lintOnSave: true,
+
+  devServer: {
+    port: process.env.VUE_APP_PORT || 9999
+  },
+
+  pluginOptions: {
+    i18n: {
+      locale: 'es',
+      fallbackLocale: 'es',
+      localeDir: 'locales',
+      enableLegacy: true,
+      runtimeOnly: false,
+      compositionOnly: false,
+      fullInstall: true
+    }
+  }
+}
